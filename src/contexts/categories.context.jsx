fdd676cfb06fcfe6be0d0029e41561bdb95efa2d
daniel <winra001@hotmail.com>
2022-04-11 21:38:31 +1000

    GraphQL queries

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,14 +1,35 @@
 import { createContext, useState, useEffect } from 'react';
 
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { gql, useQuery } from '@apollo/client';
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id,
+      title,
+      items {
+        id,
+        name,
+        price,
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
+  const {loading, error, data} = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
+  console.log('loading', loading);
+  console.log('data', data);
+
+  /*
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
@@ -17,6 +38,7 @@ export const CategoriesProvider = ({ children }) => {
 
     getCategoriesMap();
   }, []);
+  */
 
   const value = { categoriesMap };
   return (
