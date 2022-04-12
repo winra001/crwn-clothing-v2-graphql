1b4435a8dfe00ee08da56b19a82cc66ebf65d48b
daniel <winra001@hotmail.com>
2022-04-12 21:46:47 +1000

    222. Variables in useQuery and caching

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,21 +1,47 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
-
-import ProductCard from '../../components/product-card/product-card.component';
-
-import { CategoriesContext } from '../../contexts/categories.context';
+import { gql, useQuery } from '@apollo/client';
 
 import { CategoryContainer, Title } from './category.styles';
+import ProductCard from '../../components/product-card/product-card.component';
 import Spinner from '../../components/spinner/spinner.component';
+
+const GET_CATEGORY = gql(`
+  query($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id,
+      title,
+      items {
+        id,
+        name,
+        price,
+        imageUrl
+      }
+    }
+  }
+`);
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category
+    }
+  });
+
+  console.log('category data', data);
+
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if (data) {
+      const {
+        getCollectionsByTitle: { items }
+      } = data;
+      setProducts(items);
+    }
+  });
 
   return (
     <Fragment>
